0a638ad56ff5832a9a1c1082972bdd8dfa8e634d
vj.vishakhadesai <vj.vishakhadesai@gmail.com>
2026-03-06 07:44:41 +0530

    incremnetal logic implemented

--- snowflake_dbt_project/models/staging/stg_survey_main_header.sql
+++ snowflake_dbt_project/models/staging/stg_survey_main_header.sql
@@ -1,3 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(materialized='incremental') }}
 
 select * from {{source("raw", "survey_main_header")}}
+
+{% if is_incremental() %}
+    WHERE DATE > (SELECT MAX(DATE) FROM {{ this }})
+{% endif %}
